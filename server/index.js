f29822f1020da29aca2e53d99000d202662cffdc
unknown <Saliksheikh19>
2023-11-28 01:24:28 -0800

    done

--- server/index.js
+++ server/index.js
@@ -7,9 +7,15 @@ import postsRoutes from './routes/post.js';
 import categoryRoutes from './routes/categories.js';
 import  cors from 'cors'
 
-
-
 const app = express();
+
+app.use(cors(
+    {
+        origin: ["https://full-stack-blog-app-livid.vercel.app/"],
+        methods: ["POST", "GET" , "UPDATE" , "DELETE" , "PUT"],
+        credentials: true
+    }
+));
 
 const PORT = 8000;
 app.use(express.json());
